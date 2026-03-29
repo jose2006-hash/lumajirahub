3262a410583a0ec159b3b6e40c1f03af7db8dde1
jose2006-hash <jose.llanos.d@uni.pe>
2026-03-29 16:39:19 -0500

    PWA listo

--- src/main.jsx
+++ src/main.jsx
@@ -7,3 +7,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js")
+      .then(() => console.log("SW registrado"))
+      .catch(err => console.log("Error SW:", err));
+  });
+}
